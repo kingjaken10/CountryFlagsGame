dcb99654dc6bea97faddebab86f52d7900c624e9
kingjaken10 <megan.nv.ngo@gmail.com>
2025-08-04 21:15:20 -0500

    Initial commit

---
+++ CountryFlagsGame.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+from PIL import Image, ImageTkimport
+import os
+import random
+import json
+import sys
+import copy
+
+# directory containing flag images
+FLAG_DIR = "flags"
+
+# creating a list of world regions
+regions = ["Africa", "Asia", "Caribbean", "Europe", "North America", "Oceania", "South America"]
+
+
